286dd605832bbc5c9c6b28801e74b6f988d9ae4a
devlancer0328 <topdev0328@proton.me>
2024-05-13 10:50:45 +0200

    add main App component

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import Title from "@/components/Title";
+import React from "react";
+
+const App = () => {
+  return (
+    <section className="bg-white rounded-xl p-4">
+      <Title />
+    </section>
+  );
+};
+
+export default App;
