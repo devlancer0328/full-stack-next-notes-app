fa3183fb6943efe8cf10b32b6e89c82535b4d081
devlancer0328 <topdev0328@proton.me>
2024-05-13 10:51:03 +0200

    add main <App /> component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import App from "./App";
+
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main>
+      <App />
+    </main>
   );
 };
 
