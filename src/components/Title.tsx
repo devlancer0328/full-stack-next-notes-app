2f9e669a46fa685149ce0f99e2381103d27d6cbc
devlancer0328 <topdev0328@proton.me>
2024-05-13 13:27:05 +0200

    add sticky note icon

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { CiStickyNote } from "react-icons/ci";
 
 const Title = () => {
   return (
-    <div className="">
-      <h1 className="text-yellow-400">Title</h1>
+    <div className="flex">
+      <h1 className="flex items-center text-yellow-400">
+        <CiStickyNote className="me-1" />
+        Title
+      </h1>
     </div>
   );
 };
