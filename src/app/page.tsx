444abe6991cff80ba184640fa1f48041f0aae7fa
devlancer0328 <topdev0328@proton.me>
2024-05-13 10:27:48 +0200

    correct Home file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-export default Home = () => {
+const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 };
+
+export default Home;
