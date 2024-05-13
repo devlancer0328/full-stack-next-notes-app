bc9463bb13dbc59501205f20bd530ae5e4bacc68
devlancer0328 <topdev0328@proton.me>
2024-05-13 10:51:10 +0200

    add <Title /> component

---
+++ src/components/Title.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="">
+      <h1 className="text-yellow-400">Title</h1>
+    </div>
+  );
+};
+
+export default Title;
