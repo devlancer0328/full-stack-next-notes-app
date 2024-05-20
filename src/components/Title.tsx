a9f8ffd2f237f62c093be551de1f52c6df380bfd
devlancer0328 <topdev0328@proton.me>
2024-05-20 18:01:36 +0200

    styling title, add higher font-size and resize svg

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,9 +4,9 @@ import { CiStickyNote } from "react-icons/ci";
 const Title = () => {
   return (
     <div className="flex">
-      <h1 className="flex items-center text-yellow-400">
-        <CiStickyNote className="me-1" />
-        Title
+      <h1 className="flex items-center text-yellow-400 text-xl">
+        <CiStickyNote className="me-1 w-12 h-12" />
+        RetroBoard
       </h1>
     </div>
   );
