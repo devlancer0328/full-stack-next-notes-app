36bef8751db655a02add06329bd1a08a29161a0d
devlancer0328 <topdev0328@proton.me>
2024-05-20 18:21:40 +0200

    change yellow-600 to yellow-400

--- src/components/TabComponent.tsx
+++ src/components/TabComponent.tsx
@@ -36,7 +36,7 @@ const TabComponent = () => {
               href="#"
               className={`inline-flex shrink-0 items-center gap-2 border-b-2 px-1 pb-4 text-sm font-medium ${
                 activeTab === "teamb"
-                  ? "border-yellow-600 text-yellow-600"
+                  ? "border-yellow-400 text-yellow-400"
                   : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
               }`}
               onClick={() => handleTabClick("teamb")}
@@ -48,7 +48,7 @@ const TabComponent = () => {
               href="#"
               className={`inline-flex shrink-0 items-center gap-2 border-b-2 px-1 pb-4 text-sm font-medium ${
                 activeTab === "teamc"
-                  ? "border-yellow-600 text-yellow-600"
+                  ? "border-yellow-400 text-yellow-400"
                   : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
               }`}
               onClick={() => handleTabClick("teamc")}
@@ -60,7 +60,7 @@ const TabComponent = () => {
               href="#"
               className={`inline-flex shrink-0 items-center gap-2 border-b-2 px-1 pb-4 text-sm font-medium ${
                 activeTab === "teamd"
-                  ? "border-yellow-600 text-yellow-600"
+                  ? "border-yellow-400 text-yellow-400"
                   : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
               }`}
               onClick={() => handleTabClick("teamd")}
@@ -72,7 +72,7 @@ const TabComponent = () => {
               href="#"
               className={`inline-flex shrink-0 items-center gap-2 border-b-2 px-1 pb-4 text-sm font-medium ${
                 activeTab === "teame"
-                  ? "border-yellow-600 text-yellow-600"
+                  ? "border-yellow-400 text-yellow-400"
                   : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
               }`}
               onClick={() => handleTabClick("teame")}
@@ -85,7 +85,7 @@ const TabComponent = () => {
               href="#"
               className={`inline-flex shrink-0 items-center gap-2 border-b-2 px-1 pb-4 text-sm font-medium ${
                 activeTab === "teamf"
-                  ? "border-yellow-600 text-yellow-600"
+                  ? "border-yellow-400 text-yellow-400"
                   : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
               }`}
               onClick={() => handleTabClick("teamf")}
@@ -98,7 +98,7 @@ const TabComponent = () => {
               href="#"
               className={`inline-flex shrink-0 items-center gap-2 border-b-2 px-1 pb-4 text-sm font-medium ${
                 activeTab === "teamm"
-                  ? "border-yellow-600 text-yellow-600"
+                  ? "border-yellow-400 text-yellow-400"
                   : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
               }`}
               onClick={() => handleTabClick("teamm")}
